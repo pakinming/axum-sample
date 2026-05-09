22b5d7d827254716c435e25379956800587d0a77
Pakin <pakin@mfec.co.th>
2026-05-10 00:18:18 +0700

    feat: enable tracing subscriber and allow port configuration via environment variable

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,23 @@ use tokio::net::TcpListener;
 use axum::{response::Html, routing::get, Router};
 
 
-// #[tokio::main]
-// async fn main() -> Result<()> {
-//     tracing_subscriber::fmt()
-//         // For early local development.
-//         .with_target(false)
-//         .with_env_filter(EnvFilter::from_default_env())
-//         .pretty()
-//         .init();
-//     log::debug!("Hello, world!");
-//     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
-
-//     Ok(())
-// }
-
-
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt()
+        // For early local development.
+        .with_target(false)
+        .with_env_filter(EnvFilter::from_default_env())
+        .pretty()
+        .init();
+        
+    let port = std::env::var("PORT").unwrap_or_else(|_| "8080".to_string());
+    
     // build our application with a route
     let app = Router::new().route("/", get(handler));
 
+
     // run it
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080")
+    let listener = tokio::net::TcpListener::bind(&format!("0.0.0.0:{}", port))
         .await
         .unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
